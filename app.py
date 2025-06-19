088edc0988b793b05f3187c92ed5e7d57241929e
YuraForlita <forlitaura@gmail.com>
2025-06-19 17:11:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from PIL import Image
 import requests
 from io import BytesIO
 import os
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 app = Flask(__name__)
 
 def is_valid_image(url):
